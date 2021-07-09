5cd949b0b3fc4715ba2ca8dc1f933497b1d6429b
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-09 20:58:28 +0100

    Complete lesson 4 (Combine Animations)

--- Lesson-04/SavannaTours/ContentView.swift
+++ Lesson-04/SavannaTours/ContentView.swift
@@ -1,34 +1,9 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SwiftUI
 
 struct ContentView : View {
+  @State var zoomed = false
+  
+  
   var body: some View {
     VStack(spacing: 0) {
       Image("hero")
@@ -49,7 +24,7 @@ struct ContentView : View {
             .foregroundColor(.white)
         }
         .offset(
-          x: 30,
+          x: zoomed ? 500 : 30,
           y: -30
         )
         .animation(.default)
@@ -57,22 +32,24 @@ struct ContentView : View {
         GeometryReader { geometry in
           Image("thumb")
             .clipShape(
-              RoundedRectangle(cornerRadius: 500)
+              RoundedRectangle(cornerRadius: zoomed ? 40 : 500)
             )
             .overlay(
               Circle()
                 .fill(
-                  Color(white: 1, opacity: 0.4)
+                  zoomed ? Color.clear : Color(white: 1, opacity: 0.4)
                 )
                 .scaleEffect(0.8)
             )
-            .saturation(0)
+            .saturation(zoomed ? 1 : 0)
             .position(
-              x: 600,
+              x: zoomed ? geometry.frame(in: .local) . midX : 600,
               y: 50
             )
-            .scaleEffect(1 / 3)
+            .scaleEffect(zoomed ? 4/3 : 1/3)
             .shadow(radius: 10)
+            .animation(.spring())
+            .onTapGesture { zoomed.toggle() }
         }
       }
       .background(Color(white: 0.1))
