ae14392a128010aa8fa391fd20568a718a2ce446
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-10 14:51:23 +0100

    Complete lesson 8 (Interactive Animations)

--- Lesson-08/Spinner/SpinnerView.swift
+++ Lesson-08/Spinner/SpinnerView.swift
@@ -1,31 +1,3 @@
-/// Copyright (c) 2019 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SwiftUI
 
 struct SpinnerView: View {
@@ -53,6 +25,7 @@ struct SpinnerView: View {
   @State var currentIndex = -1
   @State var completed = false
   @State var isVisible = true
+  @State var currentOffset = CGSize.zero
 
   let shootUp =
     AnyTransition.offset(x: 0, y: -1000)
@@ -70,9 +43,29 @@ struct SpinnerView: View {
             )
           }
         }
+        .offset(currentOffset)
+        .blur(radius: currentOffset == .zero ? 0 : 10)
+        .gesture (
+          DragGesture().onChanged { gesture in
+            currentOffset = gesture.translation
+          }
+          .onEnded { gesture in
+            if currentOffset.height > 150 { complete() }
+          }
+        )
+        .animation(.easeInOut(duration: 1))
         .transition(shootUp)
         .onAppear(perform: animate)
       }
+    }
+  }
+  
+  func complete() {
+    guard !completed else { return }
+    completed = true
+    currentIndex = -1
+    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+      isVisible = false
     }
   }
   
@@ -85,11 +78,7 @@ struct SpinnerView: View {
       iteration += 1
       if iteration == 30 {
         timer.invalidate()
-        completed = true
-        currentIndex = -1
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-          isVisible = false
-        }
+        complete()
       }
     }
   }
